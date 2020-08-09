7b613eddab3062819cae3fcb657855045608cba3
Snake-Vink <69075989+Snake-Vink@users.noreply.github.com>
2020-08-09 21:43:28 +0800

    Create operator.cpp

---
+++ operator.cpp
@@ -0,0 +1,11 @@
+＃include <iostream>
+using namespace std;
+int main() {
+  cout << 1+2 << endl;
+  cout << 2-1 << endl;
+  cout << 1*2 << endl;
+  cout << 1/2 << endl;
+  cout << 6/3 << endl;
+  cout << 5%2 << endl;
+  return 0;
+}
